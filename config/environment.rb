4099596a2e307d4d1e7f7a538a13565cfbbc7272
isuke <isuke770@gmail.com>
2017-06-20 22:34:36 +0900

    :tada: Initial Commit

---
+++ config/environment.rb
@@ -0,0 +1,49 @@
+require 'bundler/setup'
+require 'hanami/setup'
+require 'hanami/model'
+require_relative '../lib/bluetrain_prot'
+require_relative '../apps/web/application'
+
+Hanami.configure do
+  mount Web::Application, at: '/'
+
+  model do
+    ##
+    # Database adapter
+    #
+    # Available options:
+    #
+    #  * SQL adapter
+    #    adapter :sql, 'sqlite://db/bluetrain_prot_development.sqlite3'
+    #    adapter :sql, 'postgresql://localhost/bluetrain_prot_development'
+    #    adapter :sql, 'mysql://localhost/bluetrain_prot_development'
+    #
+    adapter :sql, ENV['DATABASE_URL']
+
+    ##
+    # Migrations
+    #
+    migrations 'db/migrations'
+    schema     'db/schema.sql'
+  end
+
+  mailer do
+    root 'lib/bluetrain_prot/mailers'
+
+    # See http://hanamirb.org/guides/mailers/delivery
+    delivery :test
+  end
+
+  environment :development do
+    # See: http://hanamirb.org/guides/projects/logging
+    logger level: :debug
+  end
+
+  environment :production do
+    logger level: :info, formatter: :json
+
+    mailer do
+      delivery :smtp, address: ENV['SMTP_HOST'], port: ENV['SMTP_PORT']
+    end
+  end
+end
---
+++ lib/bluetrain_prot.rb
@@ -0,0 +1,2 @@
+module BluetrainProt
+end
